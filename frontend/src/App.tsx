e8c1cfedf0535a00c19c0c1fd621457a28b94792
okmic <okmic.g@gmail.com>
2026-05-18 22:04:47 +0300

    off logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import Logo from './components/MoonLogo/Logo';
 import DreamForm from './components/DeamForm/DeamForm';
 import DreamInterpretation from './components/DreamInterpretation/DreamInterpretation';
 import Preloader from './components/Preloader/Preloader';
@@ -36,9 +35,7 @@ function App() {
       <div>
         <div className="min-h-screen flex flex-col">
           <div className="flex-1 flex flex-col items-center justify-center py-12 sm:py-16 lg:py-20">
-            <div className="mb-8 sm:mb-12 lg:mb-16">
-              <Logo size="lg" />
-            </div>
+
             
             <DreamForm onSubmit={handleDreamSubmit} isLoading={isLoading} />
             
