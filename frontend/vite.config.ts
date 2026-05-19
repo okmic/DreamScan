4210c0296bac37419ec07d2d10d7481266eb4804
okmic <okmic.g@gmail.com>
2026-05-19 11:33:14 +0300

    edit build vite front

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/DreamScan/',
+  //base: '/DreamScan/',
   build: {
     outDir: './build',
     emptyOutDir: true
